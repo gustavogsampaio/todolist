f6e32fab94aaccddc6c92d48cf7ee77f9258a41e
unknown <gustavo.g.sampaio@aluno.senai.br>
2026-08-13 11:09:09 -0300

    Alterações aula 13/08

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -4,6 +4,8 @@
  */
 package todolist;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Aluno
@@ -15,6 +17,38 @@ public class ToDoList {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("1 - Adicionar tarefa\n" +
+"2 - Listar tarefas\n" +
+"3 - Concluir tarefa\n" +
+"4 - Excluir tarefa\n" +
+"5 - Sair");
+        
+    Scanner ler = new Scanner(System.in);
+
+        System.out.print("Qual quer acessar: ");
+        // 3. Lê uma linha inteira de texto
+        int acesso = ler.nextInt();
+        switch (acesso) {
+            case 1:
+                System.out.println("a");
+                break;
+            case 2:
+                System.out.println("b");
+                break;
+            case 3:
+                System.out.println("c");
+                break;
+            case 4:
+                System.out.println("d");
+                break;
+            case 5:
+                System.out.println("e");
+                break;
+            default:
+                System.out.println("f");
+                break;
+        }
+
     }
     
 }
